d7dd511dcfd97e069c2c50740c93de9d1a451a9d
Rualark <rualark@gmail.com>
2018-12-23 07:17:38 +0300

    Create a report view of ruleset, showing only enabled rules #15

--- ruleset.php
+++ ruleset.php
@@ -95,75 +95,110 @@ else {
   echo "<br><h2 align=center>Rule set $rs_id: $rs[rs_name]</h2>";
   echo "<p align='center'>";
   echo "<a data-toggle=tooltip data-placement=top title='Play white against this rule set' href='play.php?rs_id0=$rs_id'><img src=img/play_brown.png></a> ";
-  echo "<a data-toggle=tooltip data-placement=top title='Play this rule set for both players' href='play.php?rs_id0=$rs_id&rs_id1=$rs_id'><img src=img/play_cyan.png></a>";
-  echo "</p>";
+  echo "<a data-toggle=tooltip data-placement=top title='Play this rule set for both players' href='play.php?rs_id0=$rs_id&rs_id1=$rs_id'><img src=img/play_cyan.png></a> ";
   // Cannot change ruleset of other user (only admin)
   if ($rs['u_id'] != $uid && !$ua['u_admin']) {
     $readonly = "readonly";
   }
+  if ($act == "save" || $act == "view") {
+    if ($readonly == "") echo "<a href='ruleset.php?act=edit&rs_id=$rs_id'><img src=img/edit.png></a>";
+  }
+  echo "</p>";
 }
 
-echo "<form class='form-inline' action='ruleset.php' method='post'>";
-echo "<input type=hidden name=rs_id value='$rs_id'>";
-echo "<input type=hidden name=act value=save>";
+function show_edit_ruleset() {
+  GLOBAL $rla, $rs_id, $readonly, $rs, $rpos, $rpar0, $rpar1, $rpar2;
+  echo "<form class='form-inline' action='ruleset.php' method='post'>";
+  echo "<input type=hidden name=rs_id value='$rs_id'>";
+  echo "<input type=hidden name=act value=save>";
 
-$old_group = "";
-$collapse_container_id = 0;
-foreach ($rla as $rid => $rl) {
-  // Create collapse container
-  if ($old_group != $rl['Rgroup']) {
-    if ($old_group != "") {
-      end_collapse_container($collapse_container_id, $old_group);
+  $old_group = "";
+  $collapse_container_id = 0;
+  foreach ($rla as $rid => $rl) {
+    // Create collapse container
+    if ($old_group != $rl['Rgroup']) {
+      if ($old_group != "") {
+        end_collapse_container($collapse_container_id, $old_group);
+      }
+      ++$collapse_container_id;
+      $old_group = $rl['Rgroup'];
+      start_collapse_container($collapse_container_id, $rl['Rgroup']);
     }
-    ++$collapse_container_id;
-    $old_group = $rl['Rgroup'];
-    start_collapse_container($collapse_container_id, $rl['Rgroup']);
+    // Load defaults if new ruleset
+    if (!isset($rpos[$rid])) {
+      $rpos[$rid] = 0;
+      $rpar0[$rid] = $rl['Par0'];
+      $rpar1[$rid] = $rl['Par1'];
+      $rpar2[$rid] = $rl['Par2'];
+    }
+    echo "<tr>";
+    echo "<td>";
+    $st = $rl['Rname'];
+    $st = str_replace("XX", "</span> <input $readonly type='number' min=0 style='width: 100px' class='form-control' id='xx$rid' name='xx$rid' value='$rpar0[$rid]'> <span>", $st);
+    $st = str_replace("YY", "</span> <input $readonly type='number' min=0 style='width: 100px' class='form-control' id='yy$rid' name='yy$rid' value='$rpar1[$rid]'> <span>", $st);
+    $st = str_replace("ZZ", "</span> <input $readonly type='number' min=0 style='width: 100px' class='form-control' id='zz$rid' name='zz$rid' value='$rpar2[$rid]'> <span>", $st);
+    echo "<input data-toggle=tooltip data-placement=top title='Click to switch between probability 0% and 100%' type='checkbox' class='form-check-input' onchange=\"if (this.checked) {document.getElementById('pos$rid').value = '100';} else {document.getElementById('pos$rid').value = '0';}\" ";
+    if ($rpos[$rid] == 100) echo "checked";
+    echo "> ";
+    echo "<input data-toggle=tooltip data-placement=top title='Probability of this rule to be enabled each turn' $readonly type='number' min=0 max=100 style='width: 100px' class='form-control' id='pos$rid' name='pos$rid' value='$rpos[$rid]'> % ";
+    echo "<span data-toggle=tooltip data-placement=top title=\"$rl[Rdesc]\">";
+    echo "$st\n";
+    echo "</span>";
+    echo "</fieldset>\n";
+    echo "</div>\n";
   }
-  // Load defaults if new ruleset
-  if (!isset($rpos[$rid])) {
-    $rpos[$rid] = 0;
-    $rpar0[$rid] = $rl['Par0'];
-    $rpar1[$rid] = $rl['Par1'];
-    $rpar2[$rid] = $rl['Par2'];
-  }
-  echo "<tr>";
-  echo "<td>";
-  $st = $rl['Rname'];
-  $st = str_replace("XX", "</span> <input $readonly type='number' min=0 style='width: 100px' class='form-control' id='xx$rid' name='xx$rid' value='$rpar0[$rid]'> <span>", $st);
-  $st = str_replace("YY", "</span> <input $readonly type='number' min=0 style='width: 100px' class='form-control' id='yy$rid' name='yy$rid' value='$rpar1[$rid]'> <span>", $st);
-  $st = str_replace("ZZ", "</span> <input $readonly type='number' min=0 style='width: 100px' class='form-control' id='zz$rid' name='zz$rid' value='$rpar2[$rid]'> <span>", $st);
-  echo "<input type='checkbox' class='form-check-input' onchange=\"if (this.checked) {document.getElementById('pos$rid').value = '100';} else {document.getElementById('pos$rid').value = '0';}\" ";
-  if ($rpos[$rid] == 100) echo "checked";
-  echo "> ";
-  echo "<input $readonly type='number' min=0 max=100 style='width: 100px' class='form-control' id='pos$rid' name='pos$rid' value='$rpos[$rid]'> % ";
-  echo "<span data-toggle=tooltip data-placement=top title=\"$rl[Rdesc]\">";
-  echo "$st\n";
-  echo "</span>";
-  echo "</fieldset>\n";
-  echo "</div>\n";
-}
-end_collapse_container($collapse_container_id, $old_group);
+  end_collapse_container($collapse_container_id, $old_group);
 
-echo "<table cellpadding='3'>";
-echo "<tr>";
-echo "<td>";
-echo "<b>Rule set name:</b> <input $readonly placeholder='Enter descriptive rule set name' pattern='.{10,}' required title='10 characters minimum' class='form-control' style='width: 800px' type=text id=rs_name name=rs_name value='$rs[rs_name]'>";
-if ($rs_id) {
+  echo "<table cellpadding='3'>";
   echo "<tr>";
   echo "<td>";
-  echo "Rule set was created by $rs[u_name] at $rs[time_created]";
-  if ($rs['time_changed'] != $rs['time_created']) echo " and changed at $rs[time_changed]";
+  echo "<b>Rule set name:</b> <input $readonly placeholder='Enter descriptive rule set name' pattern='.{10,}' required title='10 characters minimum' class='form-control' style='width: 800px' type=text id=rs_name name=rs_name value='$rs[rs_name]'>";
+  echo "<tr>";
+  echo "<td>";
+  if ($readonly == "readonly") {
+    echo "<span data-toggle=tooltip data-placement=top title='Cannot change this rule set, because it belongs to a different user'><a class=\"btn btn-secondary disabled mb-2\" href='#' role=\"button\" >Save rule set</a></span>";
+  }
+  else {
+    echo "<button type='submit' class='btn btn-primary mb-2'>Save rule set</button>";
+  }
+  echo "</table>";
+  echo "</form>";
 }
-echo "<tr>";
-echo "<td>";
-if ($readonly == "readonly") {
-  echo "<span data-toggle=tooltip data-placement=top title='Cannot change this rule set, because it belongs to a different user'><a class=\"btn btn-secondary disabled mb-2\" href='#' role=\"button\" >Save rule set</a></span>";
+
+function show_view_ruleset() {
+  GLOBAL $rla, $rs_id, $readonly, $rs, $rpos, $rpar0, $rpar1, $rpar2;
+  echo "<table>";
+  foreach ($rla as $rid => $rl) {
+    if (!$rpos[$rid]) continue;
+    $st = $rl['Rname'];
+    $st = str_replace("XX", "<b>$rpar0[$rid]</b>", $st);
+    $st = str_replace("YY", "<b>$rpar1[$rid]</b>", $st);
+    $st = str_replace("ZZ", "<b>$rpar2[$rid]</b>", $st);
+    $st2 = $rl['Rdesc'];
+    $st2 = str_replace("XX", "$rpar0[$rid]", $st2);
+    $st2 = str_replace("YY", "$rpar1[$rid]", $st2);
+    $st2 = str_replace("ZZ", "$rpar2[$rid]", $st2);
+    echo "<tr>";
+    echo "<td align='right'>";
+    echo "<b>$rpos[$rid]%&nbsp;&nbsp;</b>";
+    echo "<td>";
+    echo "<span data-toggle=tooltip data-placement=top title=\"$st2\">$st</span><br>";
+  }
+  echo "</table>";
+  echo "<br>";
+}
+
+if ($act == "edit" || $act == "new") {
+  show_edit_ruleset();
 }
 else {
-  echo "<button type='submit' class='btn btn-primary mb-2'>Save rule set</button>";
+  show_view_ruleset();
 }
-echo "</table>";
-echo "</form>";
+
+if ($rs_id) {
+  echo "<p>This rule set was created by $rs[u_name] at $rs[time_created]";
+  if ($rs['time_changed'] != $rs['time_created']) echo " and changed at $rs[time_changed]";
+}
 
 include "template/footer.php";
 ?>
